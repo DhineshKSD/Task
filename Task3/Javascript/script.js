c0eac47314b3549f1d1ddbbb5be7886ab2e3958a
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-15 22:50:06 +0530

    Create script.js

---
+++ Task3/Javascript/script.js
@@ -0,0 +1,25 @@
+/*function onewayCheck() {
+    if (document.getElementById('oneway').checked) {
+        document.getElementById('ifoneway').style.visibility = 'hidden';
+    }
+    else document.getElementById('ifoneway').style.visibility = 'visible';
+
+}*/
+function changetextbox()
+{
+    if (document.getElementById('trip').value == "Oneway-Trip") {
+        document.getElementById('ifoneway').style.visibility = 'hidden';
+    } else {
+        document.getElementById('ifoneway').style.visibility = 'visible';
+    }
+}
+function updateSelect(changedSelect, selectId) {
+    var otherSelect = document.getElementById(selectId);
+    for (var i = 0; i < otherSelect.options.length; ++i) {
+      otherSelect.options[i].disabled = false;
+    }
+    if (changedSelect.selectedIndex == 0) {
+      return;
+    }
+    otherSelect.options[changedSelect.selectedIndex].disabled = true;
+  }
