66513a29fffe73497b07c70bbce49ce8bf2b9ecc
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-19 19:25:30 +0530

    Update trial.js

--- Task3/Javascript/trial.js
+++ Task3/Javascript/trial.js
@@ -5,70 +5,75 @@ flag=true;
 
 function  validate_reg() 
 {
-if((document.getElementById("name").value)=="")
-{
-    alert("please enter username");
-}
-else if(document.getElementById("pw").value=="")
-{
-    alert("please enter password");
-}
-else{  
-if(window.localStorage["username"] !=null)
- 
-user = JSON.parse(window.localStorage["username"]);
-user.push(document.getElementById("name").value);
-window.localStorage["username"] = JSON.stringify(user);
+    if((document.getElementById("name").value)=="")
+    {
+        alert("please enter username");
+    }
+    else if(document.getElementById("pw").value=="")
+    {
+        alert("please enter password");
+    }
+    else{  
+    if(window.localStorage["username"] !=null)
+    
+    user = JSON.parse(window.localStorage["username"]);
+    user.push(document.getElementById("name").value);
+    window.localStorage["username"] = JSON.stringify(user);
 
 
-if(window.localStorage["password"] !=null)
+    if(window.localStorage["password"] !=null)
 
-passwrd=JSON.parse(window.localStorage["password"]);
-passwrd.push(document.getElementById("pw").value);
-window.localStorage["password"] = JSON.stringify(passwrd);
+    passwrd=JSON.parse(window.localStorage["password"]);
+    passwrd.push(document.getElementById("pw").value);
+    window.localStorage["password"] = JSON.stringify(passwrd);
 
-if(window.localStorage["user_id"] !=null)
-
-id = JSON.parse(window.localStorage["user_id"]);
-var user_c=count+1;
-id.push(user_c);
-window.localStorage["user_id"] = JSON.stringify(id);
-
-alert("Registered Succesffuly");
-}
+    alert("Account Registered");
+    }
 }
 
 function validate_log()
 {
-var username1=JSON.parse(localStorage.getItem("username"));
-var password1=JSON.parse(localStorage.getItem("password"));
-console.log(username1);
-var usernameInput=document.getElementById("userName").value;
-var passwordInput=document.getElementById("userPw").value;
+    var username1=JSON.parse(localStorage.getItem("username"));
+    var password1=JSON.parse(localStorage.getItem("password"));
+    console.log(username1);
+    var usernameInput=document.getElementById("userName").value;
+    var passwordInput=document.getElementById("userPw").value;
 
-if(usernameInput!=""&&passwordInput!="") 
-{
-for(i=0;i<username1.length;i++){
-if(usernameInput==username1[i] &&passwordInput==password1[i]) 
+    if((document.getElementById("userName").value)=="")
     {
-    flag=true;
-    break;
-    } 
-    else 
+        alert("please enter username");
+    }
+    else if(document.getElementById("userPw").value=="")
     {
-    flag=false;
+        alert("please enter password");
     }
-    }
+    else{
+    if(usernameInput!=""&&passwordInput!="") 
+    {
+    for(i=0;i<username1.length;i++){
+    if(usernameInput==username1[i] && passwordInput==password1[i]) 
+        {
+        flag=true;
+        var id=i;
+        localStorage.setItem("user_id",id);
+        break;
+        } 
+        else 
+        {
+        flag=false;
+        }
+        }
 
     }
     if(flag==true)
     {
-    alert('Logged in');
+    alert('Logging in');
     window.location.assign("index.html");
     }
     else{
-    alert('Invalid Username and password');
+    alert('Invalid Username and Password');
     document.getElementById("userName").value="";
     document.getElementById("userPw").value="";
     }
+}
 }
