a6884296668cb7f050fe82f6c31a578b78d10fc3
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-20 16:40:28 +0530

    Update script.js

--- Task3/Javascript/script.js
+++ Task3/Javascript/script.js
@@ -23,3 +23,24 @@ function updateSelect(changedSelect, selectId) {
     }
     otherSelect.options[changedSelect.selectedIndex].disabled = true;
   }
+  var modal = document.getElementById("myModal");
+
+  var btn = document.getElementById("myBtn");
+  
+  var span = document.getElementsByClassName("close")[0];
+  
+  btn.onclick = function() 
+  {
+    modal.style.display = "block";
+  }
+  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
