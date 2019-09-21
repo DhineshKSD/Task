8578abc220aacfc63e0fc93921c3bea71acf69b9
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-21 07:59:51 +0530

    Update script.js

--- Task3/Javascript/script.js
+++ Task3/Javascript/script.js
@@ -43,4 +43,33 @@ function updateSelect(changedSelect, selectId) {
       modal.style.display = "none";
     }
   }
+
+  var contact_name= [];
+  var contact_email = [];
+  var message = [];
+
+  function contact()
+  {
+
+        if(window.localStorage["contact_name"] !=null)
+ 
+        contact_name = JSON.parse(window.localStorage["contact_name"]);
+        contact_name.push(document.getElementById("contactname").value);
+        window.localStorage["contact_name"] = JSON.stringify(contact_name);
+
+        if(window.localStorage["contact_email"] !=null)
+ 
+        contact_email = JSON.parse(window.localStorage["contact_email"]);
+        contact_email.push(document.getElementById("contactemail").value);
+        window.localStorage["contact_email"] = JSON.stringify(contact_email);
+
+        if(window.localStorage["message"] !=null)
+ 
+        message = JSON.parse(window.localStorage["message"]);
+        message.push(document.getElementById("contactmessage").value);
+        window.localStorage["message"] = JSON.stringify(message);
+
+        alert("Your Message has been successfully submitted")
+
+  }
   
