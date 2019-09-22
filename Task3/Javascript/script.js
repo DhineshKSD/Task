e148dacc0b13bc4a2450c4185c71a87e4f6d3d5d
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-22 12:10:04 +0530

    Update script.js

--- Task3/Javascript/script.js
+++ Task3/Javascript/script.js
@@ -9,7 +9,8 @@ function changetextbox()
 {
     if (document.getElementById('trip').value == "Oneway-Trip") {
         document.getElementById('ifoneway').style.visibility = 'hidden';
-    } else {
+    } 
+    else {
         document.getElementById('ifoneway').style.visibility = 'visible';
     }
 }
@@ -23,53 +24,56 @@ function updateSelect(changedSelect, selectId) {
     }
     otherSelect.options[changedSelect.selectedIndex].disabled = true;
   }
-  var modal = document.getElementById("myModal");
 
-  var btn = document.getElementById("myBtn");
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
   
-  var span = document.getElementsByClassName("close")[0];
-  
-  btn.onclick = function() 
-  {
-    modal.style.display = "block";
-  }
-  
-  span.onclick = function() {
+btn.onclick = function() 
+{
+  modal.style.display = "block";
+}
+
+span.onclick = function() 
+{
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) 
+{
+  if (event.target == modal) {
     modal.style.display = "none";
   }
-  
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+}
 
-  var contact_name= [];
-  var contact_email = [];
-  var message = [];
+var contact_name= [];
+var contact_email = [];
+var message = [];
 
   function contact()
   {
 
-        if(window.localStorage["contact_name"] !=null)
- 
-        contact_name = JSON.parse(window.localStorage["contact_name"]);
-        contact_name.push(document.getElementById("contactname").value);
-        window.localStorage["contact_name"] = JSON.stringify(contact_name);
+    if(window.localStorage["contact_name"] !=null)
 
-        if(window.localStorage["contact_email"] !=null)
- 
-        contact_email = JSON.parse(window.localStorage["contact_email"]);
-        contact_email.push(document.getElementById("contactemail").value);
-        window.localStorage["contact_email"] = JSON.stringify(contact_email);
+    contact_name = JSON.parse(window.localStorage["contact_name"]);
+    contact_name.push(document.getElementById("contactname").value);
+    window.localStorage["contact_name"] = JSON.stringify(contact_name);
 
-        if(window.localStorage["message"] !=null)
- 
-        message = JSON.parse(window.localStorage["message"]);
-        message.push(document.getElementById("contactmessage").value);
-        window.localStorage["message"] = JSON.stringify(message);
+    if(window.localStorage["contact_email"] !=null)
 
-        alert("Your Message has been successfully submitted")
+    contact_email = JSON.parse(window.localStorage["contact_email"]);
+    contact_email.push(document.getElementById("contactemail").value);
+    window.localStorage["contact_email"] = JSON.stringify(contact_email);
+
+    if(window.localStorage["message"] !=null)
+
+    message = JSON.parse(window.localStorage["message"]);
+    message.push(document.getElementById("contactmessage").value);
+    window.localStorage["message"] = JSON.stringify(message);
+
+    alert("Your Message has been successfully submitted")
 
   }
   
