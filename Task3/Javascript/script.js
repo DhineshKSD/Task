bcee148dbfb46fc1d23f899a56ffd457b7cf1d7d
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-23 19:51:40 +0530

    Update script.js

--- Task3/Javascript/script.js
+++ Task3/Javascript/script.js
@@ -1,19 +1,3 @@
-/*function onewayCheck() {
-    if (document.getElementById('oneway').checked) {
-        document.getElementById('ifoneway').style.visibility = 'hidden';
-    }
-    else document.getElementById('ifoneway').style.visibility = 'visible';
-
-}*/
-function changetextbox() //to hide date according to trip type
-{
-    if (document.getElementById('trip').value == "Oneway-Trip") {
-        document.getElementById('ifoneway').style.visibility = 'hidden';
-    } 
-    else {
-        document.getElementById('ifoneway').style.visibility = 'visible';
-    }
-}
 function updateSelect(changedSelect, selectId) //same city occurance prevent fn
 {
     var otherSelect = document.getElementById(selectId);
