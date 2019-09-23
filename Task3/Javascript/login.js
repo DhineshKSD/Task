1c5ef6f35b1961908dcb1b71fee3f972b93fcfb7
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-23 16:35:59 +0530

    Update login.js

--- Task3/Javascript/login.js
+++ Task3/Javascript/login.js
@@ -1,35 +1,57 @@
+flag =1;
+var local=1;
+localStorage.setItem('count',local);
+
 var count=[];
 var user= []; var passwrd=[];
 var id=[];
-flag=true;
+var flag_var;
 
 function  validate_reg() 
 {
-    if((document.getElementById("name").value)=="")
-    {
-        alert("please enter username");
-    }
-    else if(document.getElementById("pw").value=="")
-    {
-        alert("please enter password");
-    }
-    else
-    {  
-        if(window.localStorage["username"] !=null)
-        
-        user = JSON.parse(window.localStorage["username"]);
-        user.push(document.getElementById("name").value);
-        window.localStorage["username"] = JSON.stringify(user);
+        if((document.getElementById("name").value == "")) 
+        {
+            alert("Enter an username"); 
+            flag =0;
+        }
+        else if( document.getElementById("pw").value== "")
+        {
+            alert("Enter a password");
+            flag=0;
+        } 
+          arrayFromStorage = JSON.parse(localStorage.getItem("username"));
+        if(localStorage.getItem("username") == null)
+        {
+            flag=1;
+        }
+        else
+            {
+                for(i=0;i<arrayFromStorage.length;i++)
+                {
+                    if( document.getElementById("name").value == arrayFromStorage[i])
+                    {
+                        flag=2;
+                    }
+                }
+            }
+                if( flag ==2)
+                {
+                    alert("Username already exists...Try with other combination");
+                }
+                else if(flag==1)
+                {
+                    if(window.localStorage["username"] != null)
+                    {user = JSON.parse(window.localStorage["username"]);}
+                    user.push(document.getElementById("name").value);
+                    window.localStorage["username"] = JSON.stringify(user);
+      
+                    if(window.localStorage["password"] != null)
+                    {passwrd = JSON.parse(window.localStorage["password"]);}
+                    passwrd.push(document.getElementById("pw").value);
+                    window.localStorage["password"] = JSON.stringify(passwrd);
 
-
-        if(window.localStorage["password"] !=null)
-
-        passwrd=JSON.parse(window.localStorage["password"]);
-        passwrd.push(document.getElementById("pw").value);
-        window.localStorage["password"] = JSON.stringify(passwrd);
-
-        alert("Registered Succesffuly");
-    }
+                    alert("Registered Successfuly!");
+                }
 }
 
 function validate_log()
@@ -56,24 +78,25 @@ function validate_log()
             {
                 if(usernameInput==username1[i] && passwordInput==password1[i]) 
                 {
-                flag=true;
+                flag_var=true;
                 var id=i;
                 localStorage.setItem("user_id",id);
                 break;
                 } 
                 else 
                 {
-                flag=false;
+                flag_var=false;
                 }
             }
 
         }
-        if(flag==true)
+        if(flag_var==true)
         {
         alert('Logging in');
         window.location.assign("index.html");
         }
-        else{
+        else
+        {
         alert('Invalid Username and Password credentials');
         document.getElementById("userName").value="";
         document.getElementById("userPw").value="";
