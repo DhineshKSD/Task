84eb6ace7f8e14c1be75e9e04f98fc8de2292cba
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-09-30 19:09:41 +0530

    Update complex.js

--- Task4/Complex/complex.js
+++ Task4/Complex/complex.js
@@ -1,13 +1,11 @@
 var name = "Welcome"; //Global variable
 
-function disp_showroom()   //function to display showroom name
+(function disp_showroom()   //IIFE function to display showroom name
 {
 document.getElementById("showroom").innerHTML=("--------------------------------------------"+window.name+"---------------------------------------------<br><br>");//Prints welcome since it is a global variable accessible by window object
 document.getElementById("showroom").innerHTML+="Bikers of the world, welcome home to your dream bike shop The Moto Hub.<br>Find your right bike/scooter from various brands at The Moto Hub. View two<br>  wheelers on road price, colours, compare bikes";
-}
-disp_showroom();        
-
-
+})();
+     
 function brand(brand1,brand2,brand3) //Constructor Function
 {
     this.brand1 = brand1;
@@ -19,10 +17,10 @@ function brand(brand1,brand2,brand3) //Constructor Function
     document.getElementById("demo").innerHTML+=`-------------------------------------------------------------------------------------------------------` ;
     }
 } 
-const bikes = new brand("Royal Enfield","Yamaha","TVS","Bajaj","Honda") //object creation
+
+const bikes = new brand("Royal Enfield","Yamaha","TVS") //object creation
 bikes.display();
     
-
 class Bike_showroom                                //Class
 { 
     constructor(brand,model,year,color,price,tax,insurance) 
@@ -33,10 +31,12 @@ class Bike_showroom                                //Class
     this.color = color;
     this.ex_price = price;
     this.tax = tax;
-    this.insurance =insurance;}
+    this.insurance =insurance;
+    }
     display_bike = function ()
     {
-        if(this.brand=="Royal Enfield"){
+        if(this.brand=="Royal Enfield")
+        {
         document.getElementById("top_line1").innerHTML =`-----------------------------------------Available Variants----------------------------------------<br><br>` ;    
         document.getElementById("demo1").innerHTML+=`* Brand: ${this.brand}  <br> &nbsp&nbsp Model: ${this.model} <br> &nbsp&nbsp Year: ${this.year} model <br> &nbsp&nbsp Variant:  "${this.color}"<br> &nbsp&nbsp Price: Rs.${this.ex_price}<br><br>`;
         document.getElementById("bot_line1").innerHTML=`-------------------------------------------------------------------------------------------------------` ;
@@ -62,7 +62,8 @@ class Bike_showroom                                //Class
     Bike_showroom.prototype.display_onroadprice=function()
     {
     var original_price=this.calculate_onroadprice();           //encapsulation
-        if(this.brand=="Royal Enfield"){
+        if(this.brand=="Royal Enfield")
+        {
         document.getElementById("separator1").innerHTML =`--------------------------------------------On-Road Price--------------------------------------------<br><br>` ;    
         document.getElementById("onroad1").innerHTML+=`----- The On-road price of "${this.brand}-${this.model}" model is Rs. `+ original_price+' ------<br><br>';
         document.getElementById("onroad1").innerHTML+=` Ex-showroom Price: Rs. ${this.ex_price}<br> Road Tax: Rs. ${this.tax}<br> Insurance: Rs. ${this.insurance} <br><br>`;
@@ -73,13 +74,13 @@ class Bike_showroom                                //Class
         document.getElementById("onroad2").innerHTML+=`----- The On-road price of "${this.brand}-${this.model}" model is Rs. `+ original_price+' ------<br><br>';
         document.getElementById("onroad2").innerHTML+=` Ex-showroom Price: Rs. ${this.ex_price}<br> Road Tax: Rs. ${this.tax}<br> Insurance: Rs. ${this.insurance} <br><br>`;
         }
-        if(this.brand=="TVS"){ 
+        if(this.brand=="TVS")
+        { 
         document.getElementById("separator3").innerHTML =`--------------------------------------------On-Road Price-------------------------------------------<br><br>` ;    
         document.getElementById("onroad3").innerHTML+=`----- The On-road price of "${this.brand}-${this.model}" model is Rs. `+ original_price+' ------<br><br>';
         document.getElementById("onroad3").innerHTML+=` Ex-showroom Price: Rs. ${this.ex_price}<br> Road Tax: Rs. ${this.tax}<br> Insurance: Rs. ${this.insurance} <br><br>`;
         }
     };
-
 
 const bike1 = new Bike_showroom("Royal Enfield","Classic 350",2018,"Matte Black",165000,1760,10000); //instantiation
 
@@ -94,33 +95,13 @@ const bike3 = new Bike_showroom("Yamaha","R15 v3",2019,"Dark Knight",145000,1560
 
 const bike4 = new Bike_showroom("TVS","Apache 4V",2019,"Matte Blue",138000,1460,8700); //instantiation
 
-
 Bike_showroom.prototype.discount_price=5000;
-
-
 //IIFE
-const dp1=Bike_showroom.prototype.discounted_price=(function(){
-    var x = bike1.ex_price-bike1.discount_price;
-    //document.getElementById("discount").innerHTML+=("* The discounted price of "+bike1.brand+" "+ bike1.model+" is Rs."+" "+x+ "<br><br>");
-    return x;
-})();
-
-const dp2=Bike_showroom.prototype.discounted_price=(function(){
-    var y = bike2.ex_price-bike2.discount_price;
-    //document.getElementById("discount").innerHTML+=("* The discounted  price of "+bike2.brand+" "+ bike2.model+" is Rs."+" "+x+ "<br><br>");
-    return y;
-})();
-
-const dp3=Bike_showroom.prototype.discounted_price=(function(){
-    var z = bike3.ex_price-bike3.discount_price;
-    //document.getElementById("discount").innerHTML+=("* The discounted price of "+bike3.brand+" "+ bike3.model+" is Rs."+" "+x+ "<br><br>");
-    return z;
-})();
-
-const dp4=Bike_showroom.prototype.discounted_price=(function(){
-    var u = bike4.ex_price-bike4.discount_price;
-    //document.getElementById("discount").innerHTML+=("* The discounted price of "+bike4.brand+" "+ bike4.model+" is Rs."+" "+x+ "<br><br>");
-    return u;
+Bike_showroom.prototype.discounted_price=(function(){
+    bike1.discounted_price = bike1.ex_price-bike1.discount_price;
+    bike2.discounted_price = bike2.ex_price-bike2.discount_price;
+    bike3.discounted_price = bike3.ex_price-bike3.discount_price;
+    bike4.discounted_price = bike4.ex_price-bike4.discount_price;    
 })();
 
 Bike_showroom.prototype.display_discountprice=function()
@@ -128,18 +109,18 @@ Bike_showroom.prototype.display_discountprice=function()
     if(this.brand=="Royal Enfield")
     {
         document.getElementById("separator1x").innerHTML =`-------------------------------------------Discount Price------------------------------------------<br><br>` ;    
-        document.getElementById("discount1").innerHTML+=("* The discounted price of "+bike1.brand+" "+ bike1.model+" is Rs."+" "+dp1+ "<br> (Exclusive of tax and insurance)<br><br>");
-        document.getElementById("discount1").innerHTML+=("* The discounted price of "+bike2.brand+" "+ bike2.model+" is Rs."+" "+dp2+ "<br> (Exclusive of tax and insurance)<br><br>");
+        document.getElementById("discount1").innerHTML+=("* The discounted price of "+bike1.brand+" "+ bike1.model+" is Rs."+" "+bike1.discounted_price+ "<br> (Exclusive of tax and insurance)<br><br>");
+        document.getElementById("discount1").innerHTML+=("* The discounted price of "+bike2.brand+" "+ bike2.model+" is Rs."+" "+bike2.discounted_price+ "<br> (Exclusive of tax and insurance)<br><br>");
     }
     if(this.brand=="Yamaha")
     {
         document.getElementById("separator2x").innerHTML =`-------------------------------------------Discount Price------------------------------------------<br><br>` ; 
-        document.getElementById("discount2").innerHTML+=("* The discounted price of "+bike3.brand+" "+ bike3.model+" is Rs."+" "+dp3+ "<br> (Exclusive of tax and insurance)<br><br>");
+        document.getElementById("discount2").innerHTML+=("* The discounted price of "+bike3.brand+" "+ bike3.model+" is Rs."+" "+bike3.discounted_price+ "<br> (Exclusive of tax and insurance)<br><br>");
     }
     if(this.brand=="TVS")
     {
         document.getElementById("separator3x").innerHTML =`-------------------------------------------Discount Price------------------------------------------<br><br>` ; 
-        document.getElementById("discount3").innerHTML+=("* The discounted price of "+bike4.brand+" "+ bike4.model+" is Rs."+" "+dp4+ "<br> (Exclusive of tax and insurance)<br><br>");
+        document.getElementById("discount3").innerHTML+=("* The discounted price of "+bike4.brand+" "+ bike4.model+" is Rs."+" "+bike4.discounted_price+ "<br> (Exclusive of tax and insurance)<br><br>");
     }
 }
 
@@ -186,40 +167,39 @@ function Feedback()
                 console.log("Valid email address!");  
                 }  
             else  
-            {  
-                console.log("You have entered an invalid email address!");  
-            }
+                {  
+                    console.log("You have entered an invalid email address!");  
+                }
         return this.bikemodel[index].email;
     }
     bike_model.prototype.display = function()
     {
         document.getElementById("feedback").innerHTML +=("<br>-- Customers Feedback for " + this.sname+" --<br><br>");
-    for(var i = 0 , len = this.bikemodel.length ; i < len ; i++)
-    {
-    document.getElementById("feedback").innerHTML +=(` ${this.getName(i)} from ${this.getCity(i)} gave a rating of "${this.getFeedback(i)}" <br>`);
-    }
-    document.getElementById("feedback_sep").innerHTML += `----------------------------------------------------`;
+        for(var i = 0 , len = this.bikemodel.length ; i < len ; i++)
+        {
+        document.getElementById("feedback").innerHTML +=(` ${this.getName(i)} from ${this.getCity(i)} gave a rating of "${this.getFeedback(i)}" <br>`);
+        }
+        document.getElementById("feedback_sep").innerHTML += `----------------------------------------------------`;
     }
     Bike_A = new bike_model("Classic 350")
     Bike_B = new bike_model("Yamaha R15");
     
-    input1 = new Customer("Vasu" , "5 star" , "Bangalore","me-info@example.com");
-    input2 = new Customer("Aravind" , "4 star" , "Chennai","hi-data@example.in");
-    input3 = new Customer("Prasanna" , "4 star" , "Chennai","me-info@example.comfs");
+    user1 = new Customer("Vasu" , "5 star" , "Bangalore","me-info@example.com");
+    user2 = new Customer("Aravind" , "4 star" , "Chennai","hi-data@example.in");
+    user3 = new Customer("Prasanna" , "4 star" , "Chennai","me-info@example.comfs");
 
-    input4 = new Customer("Ashik" , "4 star" , "Trichy","me-info@example.com");
-    input5 = new Customer("Vinoth" , "4 star" , "Chennai","hi-data@example.in");
-    input6 = new Customer("Dhinesh" , "5 star" , "Thanjavur","me-info@example.comfs");
+    user4 = new Customer("Ashik" , "4 star" , "Trichy","me-info@example.com");
+    user5 = new Customer("Vinoth" , "4 star" , "Chennai","hi-data@example.in");
+    user6 = new Customer("Dhinesh" , "5 star" , "Thanjavur","me-info@example.comfs");
     
-    Bike_A.add(input1);
-    Bike_A.add(input2);
-    Bike_A.add(input3);
+    Bike_A.add(user1);
+    Bike_A.add(user2);
+    Bike_A.add(user3);
     
-    Bike_B.add(input4);
-    Bike_B.add(input5);
-    Bike_B.add(input6);
+    Bike_B.add(user4);
+    Bike_B.add(user5);
+    Bike_B.add(user6);
     
     Bike_A.display();
-    Bike_B.display();
-    
+    Bike_B.display();    
 }
